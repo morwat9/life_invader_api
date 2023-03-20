6fca9d8bc66c6784f5475bb5ad0ce0363fc4a996
Michael Orwat <morwat9@hotmail.com>
2023-03-20 04:56:14 -0700

    changed origin from local

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const uri = process.env.MONGODB_URI;
 
 const corsOptions = {
-  origin: `http://localhost:3001`,
+  origin: `https://lifeinvaderapi-production.up.railway.app/`,
 };
 
 mongoose.connect(uri, {
